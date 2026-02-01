f57eb1098ea72a684367d3f350b1f0632fb4040d
ehasingh123 <143248914+ehasingh123@users.noreply.github.com>
2026-02-02 00:39:18 +0530

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CarbonTrackerMVC.Models;
+
+namespace CarbonTrackerMVC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Meal> Meals { get; set; }
+    }
+}
