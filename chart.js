7c746067a3109f9709b53643a5970342b04cea43
ehasingh123 <143248914+ehasingh123@users.noreply.github.com>
2026-02-02 00:41:32 +0530

    Add files via upload

---
+++ chart.js
@@ -0,0 +1,27 @@
+﻿const barCtx = document.getElementById("barChart").getContext("2d");
+
+if (window.barChartInstance) {
+    window.barChartInstance.destroy();
+}
+
+window.barChartInstance = new Chart(barCtx, {
+    type: "bar",
+    data: {
+        labels: labels,
+        datasets: [{
+            label: "Emission (kg CO₂e)",
+            data: values,
+            backgroundColor: "#0d6efd"
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        resizeDelay: 200,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
